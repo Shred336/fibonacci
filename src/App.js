47c808d8b3cde4baa8e4a9264f7ced2fd13b216e
Mitchell <mavent91@gmail.com>
2022-11-18 00:49:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div name="result" id="result"></div>
+        <div name="result" id="result">
+          _-_-_-_
+        </div>
         <>is the</>
         <div id="inputDisplay"></div>
         <div>number in the fibonacci sequence</div>
--- src/Fibonacci.js
+++ src/Fibonacci.js
@@ -7,7 +7,7 @@ const Fibonacci = () => {
     let input = document.getElementById("fibnumb");
     let val = input.value;
     let inputDisplay = document.getElementById("inputDisplay");
-
+    ///fibonacci formula
     for (var i = 0; i < val; i++) {
       arr.push(arr[i] + arr[i + 1]);
     }
@@ -15,14 +15,15 @@ const Fibonacci = () => {
     console.log(arr);
     console.log(arr[val - 1]);
     console.log(inputDisplay);
+    ///pop up alert
     if (isNaN(val)) {
-      alert("That is not a number you doofus!");
+      alert("That is not a number you cungswaddle!");
     } else {
       if (val > 100 || val < 1) {
-        alert("whoa there, thats not in our range bucko");
+        alert("Whoa there fuckmaggot thats not in range!");
       }
     }
-
+    ///suffixes
     const ordinal = (inputDisplay) => {
       const ordinalRules = new Intl.PluralRules("en", {
         type: "ordinal",
@@ -44,7 +45,7 @@ const Fibonacci = () => {
   return (
     <div>
       <div id="result"></div>
-      <input id="fibnumb" placeHolder="Enter Number Here"></input>
+      <input id="fibnumb" placeHolder="Enter Number 1-100 Here"></input>
       <button onClick={fibseq}>Run</button>
     </div>
   );
